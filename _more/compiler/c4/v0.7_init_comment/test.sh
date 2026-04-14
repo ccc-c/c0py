f61bde9b6ae052d9ab368c57c585851e46ab0019
ccckmit <ccckmit@gmail.com>
2026-04-14 11:41:40 +0800

    add c4 v0.7 init comment

---
+++ _more/compiler/c4/v0.7_init_comment/test.sh
@@ -0,0 +1,18 @@
+set -x
+gcc -w -g -O1 -fno-omit-frame-pointer -fsanitize=address,undefined c4.c -o c4
+#./c4 -s test/fib.c
+./c4 test/fib.c
+./c4 hello.c
+./c4 c4.c hello.c
+./c4 c4.c c4.c hello.c
+#./c4 -s hello.c
+#./c4 c4.c hello.c
+#./c4 hello.c
+#./c4 c4.c hello.c
+#./c4 c4.c c4.c hello.c
+./c4 c4.c test/fib.c
+./c4 test/sum_for.c
+./c4 test/float.c
+./c4 test/init.c
+./c4 test/error.c
+./c4 test/comment.c
